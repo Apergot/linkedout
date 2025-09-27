2f72ff96bdd884424c7e6838c716680d17f5c09a
aperdomo <apergot95@gmail.com>
2025-09-27 22:50:11 +0100

    add helper to manage pg connection using pool

---
+++ src/pg.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg'
+import config from "./config";
+
+const pool = new Pool({
+    host: config.databaseHost(),
+    port: config.databasePort(),
+    user: config.databaseUser(),
+    password: config.databasePassword,
+    database: config.databaseName(),
+    max: 100,
+    idleTimeoutMillis: 30000,
+})
+
+export default pool
---
+++ src/pgQueryExecutor.ts
@@ -0,0 +1,22 @@
+import pool from "./pg";
+
+type PgQueryExecutor<T> = (client: any) => Promise<T>
+
+export async function withPgClient<T>(
+    executor: PgQueryExecutor<T>
+): Promise<T> {
+    const pgClient = await pool.connect()
+
+    try {
+        return await executor(pgClient)
+    } catch (err) {
+        console.log('PG Execution error', err)
+        throw err
+    } finally {
+        try {
+            pgClient.release()
+        } catch (err) {
+            console.log('PG Client release error', err)
+        }
+    }
+}

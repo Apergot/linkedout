5a44b8449f2bb97255681f8b27f994548effe80f
aperdomo <apergot95@gmail.com>
2025-09-29 19:51:39 +0100

    add script to run unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest'
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src/tests'],
   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
   transform: {
     '^.+\\.(ts|tsx)$': ['ts-jest', {}],
